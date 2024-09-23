be73d8e68a7f662dad9d8aca1394dffd600d687a
Alberto Sancho <designalbertosancho@gmail.com>
2024-09-24 01:03:41 +0200

    final

--- index.js
+++ index.js
@@ -67,13 +67,37 @@ const vegetables = [
     name: 'Nabos',
     price: '7.99€/kg',
     ref: 'Ref. 0056',
-    uds: '3-6 unidades aprox. (1,0 kg. - 1,2 kg.)',
+    uds: '3-6 unidades (aprox. 1,0 kg. - 1,2 kg.)',
     image: '/images/img-13.png'
+  },
+  {
+    name: 'Berenjenas',
+    price: '5.49€/kg',
+    ref: 'Ref. 0074',
+    uds: '3-6 unidades (aprox. 1kg)',
+    image: '/images/berenjenas.jpg'
+  },
+  {
+    name: 'Cebollas',
+    price: '1.99€/kg',
+    ref: 'Ref. 0048',
+    uds: '4 unidades',
+    image: '/images/cebollas.jpg'
   }
 ];
 
-const vegetableContainer = document.querySelector('#vegetables_container > .row');
-console.log(vegetableContainer);
+const vegetableContainer = document.querySelector('#vegetables_container');
+
+const select = `
+<select name="select" class="select me-3">
+  <option value="1" selected>1 kg</option>
+  <option value="2">2 kg</option>
+  <option value="3">3 kg</option>
+  <option value="4">4 kg</option>
+  <option value="5">5 kg</option>
+</select>
+`;
+
 vegetables.forEach(vegetal => {
   let name = vegetal.name;
   let image = vegetal.image;
@@ -81,15 +105,46 @@ vegetables.forEach(vegetal => {
   let ref = vegetal.ref;
   let uds = vegetal.uds;
   const card = 
-  `<div class="col">
-    <div class="box_section">
-      <div class="image_4"><img src="${image}"></div>
-      <h2 class="dolor_text">${price}</h2>
-      <h2 class="dolor_text">${name}</h2>
-      <h2 class="dolor_text_1">${ref}</h2>
-      <p class="tempor_text">${uds}</p>
-      <div class="buy_bt_1 active"><a href="#">Comprar</a></div>
+  `<div class="vegetables_wrapper__item card">
+    <figure class="p-3 mb-3 position-relative">
+      <img src="${image}" class="card-img-top" alt="${name}">
+      <span class="price position-absolute">${price}</span>
+    </figure>
+    <div class="body p-3 d-flex flex-wrap flex-column gap-3">
+      <div class="text-wrapper d-flex flex-wrap flex-column gap-1">
+        <h3 class="title">${name}</h3>
+        <h6 class="subtitle">${ref}</h6>
+        <p class="text m-0">${uds}</p>
+      </div> 
+      <div class="addtocart d-flex justify-content-between mt-3 align-items-stretch">
+        <div class="d-flex align-items-stretch"><span class="align-self-center me-2">Cantidad:</span>${select}</div>
+        <a href="javascript:void(0)" class="btn btn-shop" title="Añadir a la cesta"><i class="fa-solid fa-cart-shopping"></i></a>
+      </div>
     </div>
   </div>`;
 vegetableContainer.insertAdjacentHTML("beforeend", card);
 });
+
+// toggle nav
+const body = document.body;
+const open = document.querySelector('.aside-open');
+const close = document.querySelector('.aside-close');
+
+open.addEventListener('click', () => {
+  body.classList.add('aside-active');
+});
+
+close.addEventListener('click', () => {
+  if (body.classList.contains('aside-active')) {
+    body.classList.remove('aside-active');
+  }
+});
+
+// date
+const actualDate = new Date();
+const actualYear = actualDate.getFullYear();
+const copyrightText = document.querySelector('.copyright_text');
+
+copyrightText.innerHTML = `© ${actualYear} - <a href="javascript:void(0)"><strong>Alberto Sancho</strong></a>`;
+
+
--- slide.js
+++ slide.js
@@ -1,24 +1,29 @@
-let slide_width = document.querySelector('.slider').clientWidth;
-const slides_container = document.querySelector('.slider-container');
-const slides = document.querySelectorAll('.slide');
+//defino el ancho del slider en función del ancho del dispositivo
+let slide_width = document.querySelector(".slider").clientWidth;
+
+// defino las variables para el contenedor y los slides y los cuento
+const slides_container = document.querySelector(".slider-container");
+const slides = document.querySelectorAll(".slide");
 const total_slides = slides.length;
 
-const arrow_dot_container = document.querySelector('.slider__arrow-nav-container');
+// defino las variables para la navegacion
+const arrow_dot_container = document.querySelector(
+  ".slider__arrow-nav-container"
+);
+const dots_container = document.querySelector(".dot-container");
+const prev_btn = document.querySelector(".arrow--prev");
+const next_btn = document.querySelector(".arrow--next");
 
-const dots_container = document.querySelector('.dot-container');
-const prev_btn = document.querySelector('.arrow--prev');
-const next_btn = document.querySelector('.arrow--next');
-
-const slide_seconds = 4000;
+//defino los settings iniciales
+const slide_seconds = 6000;
 let current_index = 1;
 let last_index;
 let move_distance;
 let timer;
-
 let mouse_is_down = false;
 let mouse_position_x = [0, 0];
 
-
+//Calcula la distancia al siguiente slide
 function calculateNextDistance() {
   move_distance = -(current_index * slide_width);
   last_index = current_index;
@@ -32,6 +37,7 @@ function calculateNextDistance() {
   slideImages();
 }
 
+//Calcula la distancia al anterior slide
 function calculatePrevDistance() {
   if (current_index > 1) {
     last_index = current_index;
@@ -45,21 +51,19 @@ function calculatePrevDistance() {
   slideImages();
 }
 
+//muevo el slider
 function slideImages(drag_distance) {
   clearInterval(timer);
-
   const distance = drag_distance || move_distance;
-
   slides_container.style.transform = `translateX(${distance}px)`;
-  document.querySelector(`#dot-${last_index}`).classList.remove('is-active');
-  document.querySelector(`#dot-${current_index}`).classList.add('is-active');
-
+  document.querySelector(`#dot-${last_index}`).classList.remove("is-active");
+  document.querySelector(`#dot-${current_index}`).classList.add("is-active");
   timer = setInterval(calculateNextDistance, slide_seconds);
 }
 
+//muevo los thumbs
 function slideDotImages(e) {
-  const dots = this.querySelectorAll('.dot');
-
+  const dots = this.querySelectorAll(".dot");
   for (var i = 1; i <= dots.length; i++) {
     if (e.target.id == `dot-${i}`) {
       last_index = current_index;
@@ -67,56 +71,15 @@ function slideDotImages(e) {
       move_distance = -((current_index - 1) * slide_width);
     }
   }
-
   slideImages();
 }
 
-function createDraggingEffects(e) {
-
-  mouse_position_x[1] = e.clientX;
-
-  if (mouse_position_x[0] == mouse_position_x[1]) {
-    return false;
-  }
-
-  let scrolled_distance = Math.abs((mouse_position_x[0] - mouse_position_x[1]));
-  const weight = slide_width / 12;
-  
-  if (mouse_position_x[0] > mouse_position_x[1]) {
-      scrolled_distance = move_distance - scrolled_distance - weight;
-  } else {
-      scrolled_distance = move_distance + scrolled_distance + weight;
-  }
-
-  slideImages(scrolled_distance); 
-
-  console.log(current_index)
-  console.log(mouse_position_x);
-}
-
-function dragAndSlideImages() {
-  if (mouse_position_x[0] == mouse_position_x[1]) {
-    slideImages();
-    return false;
-  }
-
-  if (Math.abs(mouse_position_x[0] - mouse_position_x[1]) < 30) {
-    slideImages();
-    return false;
-  }
-
-  if (mouse_position_x[0] > mouse_position_x[1]) {
-    calculateNextDistance();
-  } else {
-    calculatePrevDistance();
-  }
-}
-
+//incicializo el slider
 function init() {
-  document.querySelector(`#dot-${current_index}`).classList.remove('is-active');
+  document.querySelector(`#dot-${current_index}`).classList.remove("is-active");
 
-  slide_width = document.querySelector('.slider').clientWidth;
-  slides.forEach(img => img.style.width = `${slide_width}px`);
+  slide_width = document.querySelector(".slider").clientWidth;
+  slides.forEach((img) => (img.style.width = `${slide_width}px`));
   slides_container.style.width = `${slide_width * total_slides}px`;
 
   current_index = total_slides;
@@ -125,41 +88,9 @@ function init() {
 
 init();
 
-next_btn.addEventListener('click', calculateNextDistance);
-prev_btn.addEventListener('click', calculatePrevDistance);
-dots_container.addEventListener('click', slideDotImages);
-window.addEventListener('resize', init);
-
-
-arrow_dot_container.addEventListener('mousedown', (e) => {
-  mouse_is_down = true;
-
-  clearInterval(timer);
-
-  mouse_position_x = [e.clientX, 0];
-  arrow_dot_container.style.cursor = '-webkit-grabbing';
-  console.log(mouse_position_x);
-})
-
-arrow_dot_container.addEventListener('mousemove', (e) => {
-  if (!mouse_is_down) return false; 
-  createDraggingEffects(e);
-})
-
-arrow_dot_container.addEventListener('mouseup', (e) => {
-  mouse_position_x[1] = e.clientX;
-  dragAndSlideImages(e);
-  mouse_is_down = false;
-  arrow_dot_container.style.cursor = '';
-
-  console.log(current_index);
-})
-
-arrow_dot_container.addEventListener('mouseleave', () => {
-  if (!mouse_is_down) return false;
-  
-  mouse_position_x = [0, 0];
-  dragAndSlideImages();
-  mouse_is_down = false;
-  arrow_dot_container.style.cursor = ''; 
-})
+//Se añader los eventos a las funciones
+next_btn.addEventListener("click", calculateNextDistance);
+prev_btn.addEventListener("click", calculatePrevDistance);
+dots_container.addEventListener("click", slideDotImages);
+//Si hay un resize hay que recalcular las distancias
+window.addEventListener("resize", init);
